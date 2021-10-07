86f5ab9d8f67cc58c57a50e19555e437422696b8
giau2710 <huynhvangiau2710@gmail.com>
2021-10-07 11:44:25 +0700

    Create System time

---
+++ Introduction/src/show_the_time/lesson1/Mainn.java
@@ -0,0 +1,10 @@
+package show_the_time.lesson1;
+
+import java.util.Date;
+
+public class Mainn {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(" Now is " + now);
+    }
+}
--- SystemTime/src/main/java/SystemTime.java
+++
@@ -1,5 +0,0 @@
-public class SystemTime {
-    public static void main(String[] args) {
-        System.out.println("Hello Giàu");
-    }
-}
